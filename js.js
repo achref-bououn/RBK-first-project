9fb3aeb04e1edd97930b39f4c535429b153d3580
achref-bououn <achref.bououn@gmail.com>
2020-07-12 14:47:20 +0100

    js-attempt

--- js.js
+++ js.js
@@ -11,9 +11,25 @@ $(document).ready(function(){
 	$('.question').hide();
 	$('#q1').show();
 
-
-	
+	$('.question #submit').on('click',function(){
+		var now=$(this).parents('form:first').data('question');
+		var next=$(this).parents('form:first').data('question')+1;
+		checkAnswer(''+next);
+		$('.question').hide();
+		$('#q'+next).show();
+		
+	});
 	return false;
 	});
-
+return false;
 });
+
+function checkAnswer(n){
+	var check=$('input[name'+n+']:cheked').val();
+	if (check===solutions[n-1]) {
+		score++;
+	}
+	if(n===10){
+		$('#result').html('<h3>Your final score is: '+score+'out of 10</h3><a href="html.html"></a>')
+	}
+}
