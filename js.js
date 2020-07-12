d5aa16c0ed9376ab1f47275d4b097265aaa7ca77
achref-bououn <achref.bououn@gmail.com>
2020-07-12 16:48:47 +0100

    check-answers

--- js.js
+++ js.js
@@ -1,5 +1,5 @@
 var solutions=['c','a','b','d','c','a','b','c','a','a'];
-var players={};
+var players=[];
 var score=0;
 
 $(document).ready(function(){
@@ -8,28 +8,62 @@ $(document).ready(function(){
 
 	$('#init #start').on('click',function(){
 	$('#init').hide();
-	$('.question').hide();
-	$('#q1').show();
-
-	$('.question #submit').on('click',function(){
-		var now=$(this).parents('form:first').data('question');
-		var next=$(this).parents('form:first').data('question')+1;
-		checkAnswer(''+next);
+	$('#q1').fadeIn(300);
+	$('#q1 #submit').on('click',function(){
 		$('.question').hide();
-		$('#q'+next).show();
-		
-	});
+		$('#q2').fadeIn(300);
+		return false;
+	})
+	$('#q2 #submit').on('click',function(){
+		$('.question').hide();
+		$('#q3').fadeIn(300);
+		return false;
+	})
+	$('#q3 #submit').on('click',function(){
+		$('.question').hide();
+		$('#q4').fadeIn(300);
+		return false;
+	})
+	$('#q4 #submit').on('click',function(){
+		$('.question').hide();
+		$('#q5').fadeIn(300);
+		return false;
+	})
+	$('#q5 #submit').on('click',function(){
+		$('.question').hide();
+		$('#q6').fadeIn(300);
+		return false;
+	})
+	$('#q6 #submit').on('click',function(){
+		$('.question').hide();
+		$('#q7').fadeIn(300);
+		return false;
+	})
+	$('#q7 #submit').on('click',function(){
+		$('.question').hide();
+		$('#q8').fadeIn(300);
+		return false;
+	})
+	$('#q8 #submit').on('click',function(){
+		$('.question').hide();
+		$('#q9').fadeIn(300);
+		return false;
+	})
+	$('#q9 #submit').on('click',function(){
+		$('.question').hide();
+		$('#q10').fadeIn(300);
+		return false;
+	})
+	$('#q10 #submit').on('click',function(){
+		$('.question').hide();
+		$('#result').fadeIn(300);
+		return false;
+	})
 	return false;
 	});
-return false;
-});
+	
+}); 
 
-function checkAnswer(n){
-	var check=$('input[name'+n+']:cheked').val();
-	if (check===solutions[n-1]) {
-		score++;
-	}
-	if(n===10){
-		$('#result').html('<h3>Your final score is: '+score+'out of 10</h3><a href="html.html"></a>')
-	}
-}
+
+
+	
