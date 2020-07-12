d04da70de3231040505d92fb0ee4ffc0468b7433
achref-bououn <achref.bououn@gmail.com>
2020-07-12 13:14:16 +0100

    js

--- js.js
+++ js.js
@@ -1,1 +1,19 @@
-console.log('hi')
+var solutions=['c','a','b','d','c','a','b','c','a','a'];
+var players={};
+var score=0;
+
+$(document).ready(function(){
+	$('.question').hide();
+	$('#init').show();
+
+	$('#init #start').on('click',function(){
+	$('#init').hide();
+	$('.question').hide();
+	$('#q1').show();
+
+
+	
+	return false;
+	});
+
+});
